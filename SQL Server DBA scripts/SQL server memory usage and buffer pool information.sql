0f880295035a6c679515ce9454bc5b0723c5f11b
Sayan Dey <40330595+Snippyguy@users.noreply.github.com>
2026-02-09 14:14:21 +0530

    SQL server memory usage and buffer pool information

---
+++ SQL Server DBA scripts/SQL server memory usage and buffer pool information.sql
@@ -0,0 +1,30 @@
+﻿--SQL server memory usage and buffer pool information
+
+SELECT 
+ cpu_count,
+ CAST(physical_memory_kb / 1024.0 AS DECIMAL(10, 2)) AS 'Total_RAM_of_OS(MB)',
+ CAST(committed_target_kb / 1024.0 AS DECIMAL(10, 2)) AS 'Total_RAM_visible_to_SQLServer(MB)',
+ CAST(committed_kb / 1024.0 AS DECIMAL(10, 2)) AS 'Total_RAM_used_by_SQLServer(MB)',
+ CAST(
+ (SELECT SUM(CASE WHEN is_modified = 1 THEN 1 ELSE 0 END) * 8 / 1024.0
+ FROM sys.dm_os_buffer_descriptors
+ WHERE database_id <> 32767) AS DECIMAL(10, 2)
+ ) AS 'Total_dirty_buffers_of_SQLServer(MB)',
+ CAST(
+ (SELECT SUM(CASE WHEN is_modified = 1 THEN 0 ELSE 1 END) * 8 / 1024.0
+ FROM sys.dm_os_buffer_descriptors
+ WHERE database_id <> 32767) AS DECIMAL(10, 2)
+ ) AS 'Total_clean_buffers_of_SQLServer(MB)',
+ sqlserver_start_time,
+ DATEDIFF(MINUTE, sqlserver_start_time, GETDATE()) AS serverUptime_min,
+ virtual_machine_type,
+ virtual_machine_type_desc,
+ socket_count,
+ cores_per_socket
+FROM sys.dm_os_sys_info;
+
+/*Why this is useful:
+👉 Dirty Buffers: Data in memory that is changed and needs to be saved to disk(By CHECKPOINT).
+👉Clean Buffers: Data in memory that is not changed.
+This query helps you see if your server has memory problems.
+*/
